182d2625f6def87405848278ef959bbb3ee6deb5
harshavardhan8118 <64030338+harshavardhan8118@users.noreply.github.com>
2022-08-16 23:00:07 +0530

    Add files via upload

---
+++ resume.js
@@ -0,0 +1,90 @@
+
+var basics = {
+      name: "Harshavardhan",
+      email: "harshavardhan.ap97@gmail.com",
+      phone: "7708788004",
+      summary: "I am harshavardhan graduated from VCET in B.E.Biomedical Engineering",
+}
+var  location = {
+        address: "3A,1st Floor",
+        postalCode: "Mahadevpura",
+        city: "BLR",
+        countryCode: "IN",
+        region: "KA"
+      }
+
+
+    var profiles = {
+        network: "facebook",
+        username: "harshavardhan",
+        url: "https://facebook.com/harshavardhan"
+      }
+  
+    var  work = { 
+    name: "Amazon",
+      position: "Catalog Associate",
+      url: "https://amazon.com",
+      startDate: "08-06-2020",
+      endDate: "present",
+      summary: "The job looks into the wide field of MACHINE LEARNING, cocepts of dealing with entities and intents"
+      }
+    
+      var volunteer = {
+      organization: "Rotract",
+      position: "Vice-president",
+      url: "https://www.rotract.com/"
+      }
+
+      var education = {
+      institution: "Anna University",
+      url: "https://annauniversity.com/",
+      area: "Biomedical Engineering",
+      studyType: "Bachelor",
+      startDate: "20-06-2014",
+      endDate: "20-07-2018",
+      score: "5.6"
+      }
+
+      var certificates = {
+      name: "IBM Watson",
+      date: "20-07-2020",
+      issuer: "IBM"
+    }
+
+    var skills = {
+      "name": "Hard Skills",
+      "level": "Intermediate",
+      "keywords": [
+        "HTML",
+        "CSS",
+        "Python",
+        "SQL",
+        "IBM Watson",        
+        "JavaScript"
+      ]
+    }
+
+    var languages = {
+        language: "English",
+        fluency: "Native speaker",
+        language: "Tamil",
+        fluency: "Native speaker",
+        language: "Hindi",
+        fluency: "Intermediate"        
+    }
+
+    var interests = {
+        name: "Swimming",
+        name: "Mixology",
+        name: "Writing"
+      }
+    
+  console.log(basics);
+  console.log(location);
+  console.log(profiles);
+  console.log(volunteer);
+  console.log(education);
+  console.log(certificates);
+  console.log(skills);
+  console.log(languages);
+  console.log(interests);
