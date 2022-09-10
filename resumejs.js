159185df46730034a609f5b47e49211957b289ab
harshavardhan8118 <64030338+harshavardhan8118@users.noreply.github.com>
2022-09-11 04:29:11 +0530

    Add files via upload

---
+++ resumejs.js
@@ -0,0 +1,108 @@
+console.log("BASICS:");
+var basi = {
+    name: ":   Harshavardhan",
+    email: ":   harshavardhan@gmail.com",
+    "phone  :": 77087880,
+    summary: ":   I am harshavardhan graduated in B.E.Biomedical Engineering",
+};
+
+for (var basie in basi){
+    console.log(basie,basi[basie]);
+}
+console.log("LOCATION:");
+var  locat = {
+    address: ":   3A,1st Floor",
+    postalCode: ":   Mahadevpura",
+    city: ":   BLR",
+    countryCode: ":   IN",
+    region: ":   KA",
+    
+  };
+  for (var loca in locat){
+    console.log(loca,locat[loca]);
+}
+console.log("PROFILE:");
+ var  profi = {
+    network: ":   facebook",
+    username: ":   harshavardhan",
+    url: ":   https://facebook.com/harshavardhan"
+  };
+  for (var prof in profi){
+    console.log(prof,profi[prof]);
+}
+
+  
+console.log("WORK:");
+  var  work = { 
+    name: ":   Amazon",
+    position: ":   Catalog Associate",
+    url: ":   https://amazon.com",
+    startDate: ":   08-06-2020",
+    endDate: ":   present",
+    summary: ":   The job looks into the wide field of MACHINE LEARNING, cocepts of dealing with entities and intents"
+  };
+  for (var worke  in work ){
+    console.log(worke ,work [worke ]);
+}
+console.log("VOLUNTEER:"); 
+  var volunteer = {
+    organization: ":   Rotract",
+    position: ":   Vice-president",
+    url: ":   https://www.rotract.com/"
+  };
+  for (var olunteer in volunteer){
+    console.log(olunteer,volunteer[olunteer]);
+}
+console.log("EDUCATION:"); 
+  var education = {
+    institution: ":   Anna University",
+    url: ":   https://annauniversity.com/",
+    area: ":   Biomedical Engineering",
+    studyType: ":   Bachelor",
+    startDate: ":   20-06-2014",
+    endDate: ":   20-07-2018",
+    score: ":   5.6"
+  };
+  for (var educa in education){
+    console.log(educa,education[educa]);
+}
+console.log("SKILLS:");
+  var skills = {
+    "name": ":   Hard Skills",
+    "level": ":   Intermediate",
+    "Language": ["HTML","CSS","Python","SQL","IBM Watson","JavaScript"]
+  };
+  for (var skill in skills){
+    console.log(skill,skills[skill]);
+}
+console.log("CERTIFICATES:");
+  var certificates = {
+    name: ":   IBM Watson",
+    date: ":   20-07-2020",
+    issuer: ":   IBM"
+  };
+  for (var certificatese in certificates){
+    console.log(certificatese,certificates[certificatese]);
+}
+  
+  
+console.log("LANGUAGES:"); 
+  var languages = {
+    language: ":   English",
+    fluency: ":   Native speaker",
+    language: ":   Tamil",
+    fluency: ":   Native speaker",
+    language: ":   Hindi",
+    fluency: ":   Intermediate"        
+  };
+  for (var language in languages){
+    console.log(language,languages[language]);
+}
+console.log("INTERESTS:");
+  var interests = {
+    Hobbies: ["Swimming","Mixology","Writing"]
+  };
+  for (var interest in interests){
+    console.log(interest,interests[interest]);
+}
+
